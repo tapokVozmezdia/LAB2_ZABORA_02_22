f252ab580839fe73aca3db481d611176500b18dc
tapokVozmezdia <112655065+tapokVozmezdia@users.noreply.github.com>
2022-12-12 01:59:08 +0300

    Create funcs.h

---
+++ funcs.h
@@ -0,0 +1,16 @@
+#ifndef BITCALLABFUNC_H
+#define BITCALLABFUNC_H
+
+void strToAr(char str[16], int *bitNumber);
+
+void printBin(int *binNum);
+
+void bitAdd(int *firstOperand, int *secondOperand, int *result);
+
+void bitMultiply(int *firstOperand, int *secondOperand, int *result);
+
+void bitXOR(int *firstOperand, int *secondOperand, int *result);
+
+void bitInverse(int *operand, int *result);
+
+#endif // BITCALLABFUNC_H
